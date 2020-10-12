76655bc24d9838907c8ad9baac4d8d1442cba84a
colinthomas-z80 <cthomas0687@gmail.com>
2020-10-11 23:25:23 -0400

    Fix refactor issue

--- src/chip8.c
+++ src/chip8.c
@@ -1,11 +1,11 @@
 #include "header.h"
 
-static void push(Chip8machine *, uint16_t);
-static uint16_t pop(Chip8machine *);
+static void push(Chip8State *);
+static uint16_t pop(Chip8State *);
 
-Chip8machine* InitChip8(void)
+Chip8State* InitChip8(void)
 {
-    Chip8machine* s = calloc(sizeof(Chip8machine), 1);
+    Chip8State* s = calloc(sizeof(Chip8State), 1);
 
     s->memory = calloc(1024*4, 1);
     s->screen = &s->memory[0xf00];
@@ -15,7 +15,7 @@ Chip8machine* InitChip8(void)
     return s;
 }
 
-void emulate(Chip8machine *machine)
+void emulate(Chip8State *machine)
 {
     uint8_t *code = &machine->memory[machine->PC];
 
@@ -61,11 +61,13 @@ void emulate(Chip8machine *machine)
             {
                 uint8_t reg = code[0] & 0x0f;
                 machine->V[reg] = code[1];
+                machine->PC += 2;
             }; break;
         case 0x07:
             {
                 uint8_t reg = code[0] & 0x0f;
                 machine->V[reg] += code[1];
+                machine->PC += 2;
             }
         case 0x08:
             {
@@ -87,7 +89,7 @@ void emulate(Chip8machine *machine)
                         uint8_t reg1 = code[0] & 0x0f;
                         uint8_t reg2 = code[1] >> 4;
                         machine->V[reg1] = machine->V[reg1] & machine->V[reg2];
-                    }; break
+                    }; break;
                     case 0x03:
                     {
                         uint8_t reg1 = code[0] & 0x0f;
@@ -120,17 +122,67 @@ void emulate(Chip8machine *machine)
                         machine->V[0x0f] = lsb;
                         machine->V[reg1] >> 1;
                     }; break;
+                    case 0x07:
+                    {
+                        uint8_t reg1 = code[0] & 0x0f;
+                        uint8_t reg2 = code[1] >> 4;
+                        int borrow = machine->V[reg2] > machine->V[reg1];
+                        machine->V[0x0f] = borrow;
+                        machine->V[reg1] = machine->V[reg2] - machine->V[reg1];
+                    }; break;
+                    case 0x0e:
+                    {
+                        uint8_t reg1 = code[0] & 0x0f;
+                        uint8_t msb = machine->V[reg1] >> 7;
+                        machine->V[0x0f] = msb;
+                        machine->V[reg1] << 1;
+                    }; break;
+                }
+                machine->PC += 2;
+            }; break;
+            case 0x09:
+                {
+                    uint8_t reg1 = code[0] & 0x0f;
+                    uint8_t reg2 = code[1] >> 4;
+                    if(machine->V[reg1] != machine->V[reg2])
+                        machine->PC += 2;
+                    machine->PC+=2;
+                }; break;
+            case 0x0a:
+                {
+                    machine->I = ((code[0] & 0x0f) << 8) | code[1];
+                    machine->PC += 2;
+                }; break;
+            case 0x0b:
+                {
+                    uint16_t offset = machine->V[0x00];
+                    machine->PC = (((code[0] & 0x0f) << 8) | code[1]) + offset;
+                }; break;
+            case 0x0c:
+                {
+                   uint8_t reg1 = code[0] & 0x0f;
+                   machine->V[reg1] = rand() & code[1];
+                   machine->PC += 2; 
+                }
+            case 0x0d:
+                {
+
                 }
 
-            }
+                
         
     }
 
 }
 
-void push(Chip8machine *machine)
+static void push(Chip8State *machine)
 {
     machine->SP -= 2;
     machine->memory[machine->SP] = ((machine->PC+2) & 0xFF00) >> 8;
     machine->memory[machine->SP+1] = (machine->PC+2) & 0xFF;
+}
+
+static uint16_t pop(Chip8State *machine)
+{
+
 }
